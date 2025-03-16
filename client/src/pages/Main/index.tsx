b7f698429d0c0f212425bc1cc1f5a0b77608a2c5
kimsiyeon0223 <ksyeon0223@gmail.com>
2025-03-16 18:57:04 +0900

    feat: 메인 페이지 퍼블리싱

---
+++ client/src/pages/Main/index.tsx
@@ -0,0 +1,110 @@
+import * as S from "./style";
+import Header from "../../shared/components/Header";
+import WholeBtn from "../../shared/components/WholeBtn";
+import Status from "../../shared/components/Status";
+
+const Main = () => {
+  const StudentDetail = [
+    {
+      studentId: 1,
+      name: "강민지",
+      roomId: 304,
+      building: "A",
+      gender: "여",
+      enterStatus: 1,
+      enterTime: "3월 12일 (일) 6:40:55",
+    },
+    {
+      studentId: 2,
+      name: "김시연",
+      roomId: 304,
+      building: "A",
+      gender: "여",
+      enterStatus: 1,
+      enterTime: "3월 12일 (일) 6:40:55",
+    },
+    {
+      studentId: 3,
+      name: "정소울",
+      roomId: 405,
+      building: "A",
+      gender: "남",
+      enterStatus: 1,
+      enterTime: "3월 12일 (일) 6:40:55",
+    },
+    {
+      studentId: 4,
+      name: "조윤소",
+      roomId: 205,
+      building: "B",
+      gender: "여",
+      enterStatus: 1,
+      enterTime: "3월 12일 (일) 6:40:55",
+    },
+    {
+      studentId: 5,
+      name: "강민지",
+      roomId: 305,
+      building: "B",
+      gender: "남",
+      enterStatus: 1,
+      enterTime: "3월 12일 (일) 6:40:55",
+    },
+  ];
+
+  const getJoinStatus = (enterStatus: number) => {
+    switch (enterStatus) {
+      case 1:
+        return "입소완료";
+      case 2:
+        return "입소전";
+      case 3:
+        return "미입소";
+      default:
+        return "";
+    }
+  };
+
+  return (
+    <S.Layout>
+      <Header />
+      <S.MainContainer>
+        <S.RightContainer>
+          <S.TextContainer>
+            <S.Title>출석리스트</S.Title>
+            <S.Date>2025년 3월 첫째주</S.Date>
+          </S.TextContainer>
+          <S.RightWholeBtnContainer>
+            <WholeBtn text="전체" />
+            <WholeBtn text="남학생" />
+            <WholeBtn text="여학생" />
+            <WholeBtn text="미입소자" />
+          </S.RightWholeBtnContainer>
+          <S.StudentListContainer>
+            {StudentDetail.map((student) => (
+              <Status
+                key={student.studentId}
+                joinStatus={getJoinStatus(student.enterStatus)}
+                name={student.name}
+                roomNumber={student.roomId}
+                building={student.building}
+                date={student.enterTime}
+              />
+            ))}
+          </S.StudentListContainer>
+        </S.RightContainer>
+        <S.Line />
+        <S.LeftContainer>
+          <S.WholeBtn>전체</S.WholeBtn>
+          <S.FloorBtn>A동 기숙사 2층</S.FloorBtn>
+          <S.FloorBtn>A동 기숙사 3층</S.FloorBtn>
+          <S.FloorBtn>B동 기숙사 3층</S.FloorBtn>
+          <S.FloorBtn>B동 기숙사 4층</S.FloorBtn>
+          <S.NoJoinBtn>미입소 등록</S.NoJoinBtn>
+        </S.LeftContainer>
+      </S.MainContainer>
+    </S.Layout>
+  );
+};
+
+export default Main;
---
+++ client/src/pages/Main/style.ts
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import { Pretendard } from "../../shared/style/font";
+import theme from "../../shared/style/theme";
+
+export const Layout = styled.main`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MainContainer = styled.div`
+  display: flex;
+  padding: 60px 0 200px 0;
+  justify-content: center;
+`;
+
+export const RightContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-right: 32px;
+`;
+export const TextContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+export const Title = styled.span`
+  ${Pretendard.Title1}
+  color: ${theme.black};
+`;
+export const Date = styled.span`
+  ${Pretendard.Body3}
+  color: ${theme.gray300};
+`;
+export const RightWholeBtnContainer = styled.div`
+  padding-top: 20px;
+  border-bottom: 1px solid ${theme.gray100};
+`;
+export const StudentListContainer = styled.div`
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+export const Line = styled.div`
+  border-right: 1px solid ${theme.gray100};
+`;
+export const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 32px;
+  gap: 20px;
+  align-items: center;
+`;
+export const WholeBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  ${Pretendard.Bnt2}
+  color: ${theme.blue};
+`;
+export const FloorBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  ${Pretendard.Bnt2}
+  color: ${theme.gray200};
+`;
+export const NoJoinBtn = styled.button`
+  border: none;
+  background-color: ${theme.red};
+  ${Pretendard.Bnt2}
+  cursor: pointer;
+  color: ${theme.white};
+  padding: 12px 22.5px;
+  border-radius: 8px;
+`;
