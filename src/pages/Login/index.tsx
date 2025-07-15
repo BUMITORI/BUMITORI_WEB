46a1223cd6b2df2f34f4de14720ebaf734446794
JeongSuHwan23 <jeongsuhwan@jeongsuhwan-ui-MacBookAir-7.local>
2025-07-16 07:53:02 +0900

    login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,12 +3,17 @@ import Logo from "../../assets/logo.svg";
 import GoogleLogo from "../../assets/googleLogo.svg";
 
 const Login = () => {
+  const handleGoogleLogin = () => {
+    const redirectUri = `https://bumitori.duckdns.org/oauth2/authorization/google`;
+    window.location.href = redirectUri;
+  };
+
   return (
     <S.Layout>
       <S.MainContainer>
-        <img src={Logo} />
-        <S.LoginContainer>
-          <S.GoogleLogoImg src={GoogleLogo} />
+        <img src={Logo} alt="로고" />
+        <S.LoginContainer onClick={handleGoogleLogin}>
+          <S.GoogleLogoImg src={GoogleLogo} alt="Google 로고" />
           <S.GoLogin>Google로 로그인</S.GoLogin>
         </S.LoginContainer>
         <S.Precautions>BSSM 학교 계정으로만 로그인이 가능합니다.</S.Precautions>
